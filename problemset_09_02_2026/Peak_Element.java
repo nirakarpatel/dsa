3590047d4ded1fbcd361a091296f127745342fb7
Nirakar Patel <161930923+nirakarpatel@users.noreply.github.com>
2026-02-11 17:14:13 +0530

    Fix loop condition in findPeakElement method

--- problemset_09_02_2026/Peak_Element.java
+++ problemset_09_02_2026/Peak_Element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        for(int i =0;i<nums.length;i++){
+        for(int i =0;i<nums.length-1;i++){
             if(nums[i]>nums[i+1])
             return i;
         }
